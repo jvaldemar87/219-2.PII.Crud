0f37e42b9145e7078a47da5644014c68d211aefa
troya14 <33033201+troya14@users.noreply.github.com>
2018-12-05 04:38:58 -0600

    Add files via upload
    Evento que hace  aumentar en tama;o una imagen si el mouse esta sobre el y regresar a su tama;o despues de  no estar

---
+++ appmar/javascript/Agrandar.js
@@ -0,0 +1,4 @@
+function Agrandar(x) {
+    x.style.height = "200px";
+    x.style.width = "200px";
+}
---
+++ appmar/javascript/Normal.js
@@ -0,0 +1,4 @@
+function Normal(x) {
+    x.style.height = "100px";
+    x.style.width = "100px";
+}
